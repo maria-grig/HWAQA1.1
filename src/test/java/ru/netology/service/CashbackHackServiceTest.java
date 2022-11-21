17d573ed7bf083882b769e34450baf83396cc1a0
MariaG <MGrigorenko@bellintegrator.com>
2022-11-21 18:56:31 +0300

    auto-reformat code

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -5,63 +5,63 @@ import org.testng.annotations.Test;
 
 public class CashbackHackServiceTest {
 
-        public CashbackHackService service = new CashbackHackService();
+    public CashbackHackService service = new CashbackHackService();
 
-        @Test
-        public void shouldCalculateWhenOneHundredRubleLackToThousand(){
+    @Test
+    public void shouldCalculateWhenOneHundredRubleLackToThousand() {
 
-                int expected = 100;
-                int actual = service.remain(900);
-                Assert.assertEquals(actual, expected);
-        }
+        int expected = 100;
+        int actual = service.remain(900);
+        Assert.assertEquals(actual, expected);
+    }
 
-        @Test
-        public void shouldCalculateWithFullThousand(){
+    @Test
+    public void shouldCalculateWithFullThousand() {
 
-                int expected = 0;
-                int actual = service.remain(1000);
-                Assert.assertEquals(actual, expected);
-        }
+        int expected = 0;
+        int actual = service.remain(1000);
+        Assert.assertEquals(actual, expected);
+    }
 
-        @Test
-        public void shouldCalculateWhenNineHundredNinetyNineLackToTwoThousand(){
+    @Test
+    public void shouldCalculateWhenNineHundredNinetyNineLackToTwoThousand() {
 
-                int expected = 999;
-                int actual = service.remain(1001);
-                Assert.assertEquals(actual, expected);
-        }
+        int expected = 999;
+        int actual = service.remain(1001);
+        Assert.assertEquals(actual, expected);
+    }
 
-        @Test
-        public void shouldCalculateWhenOneRubleLackToThousand(){
+    @Test
+    public void shouldCalculateWhenOneRubleLackToThousand() {
 
-                int expected = 1;
-                int actual = service.remain(999);
-                Assert.assertEquals(actual, expected);
-        }
+        int expected = 1;
+        int actual = service.remain(999);
+        Assert.assertEquals(actual, expected);
+    }
 
-        @Test
-        public void shouldCalculateWhenNineHundredNinetyNineLackToThousand(){
+    @Test
+    public void shouldCalculateWhenNineHundredNinetyNineLackToThousand() {
 
-                int expected = 999;
-                int actual = service.remain(1);
-                Assert.assertEquals(actual, expected);
-        }
+        int expected = 999;
+        int actual = service.remain(1);
+        Assert.assertEquals(actual, expected);
+    }
 
-        @Test
-        public void shouldCalculateWhenFullSumLack(){
+    @Test
+    public void shouldCalculateWhenFullSumLack() {
 
-                int expected = 1000;
-                int actual = service.remain(0);
-                Assert.assertEquals(actual, expected);
-        }
+        int expected = 1000;
+        int actual = service.remain(0);
+        Assert.assertEquals(actual, expected);
+    }
 
-        @Test
-        public void shouldCalculateWithNegativeSum(){
+    @Test
+    public void shouldCalculateWithNegativeSum() {
 
-                int expected = 1001;
-                int actual = service.remain(-1);
-                Assert.assertEquals(actual, expected);
-        }
+        int expected = 1001;
+        int actual = service.remain(-1);
+        Assert.assertEquals(actual, expected);
+    }
 
 
 }
