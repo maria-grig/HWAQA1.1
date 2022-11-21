31d1b4172d7f7f93182b1c14f87298def4e2a49d
MariaG <MGrigorenko@bellintegrator.com>
2022-11-21 18:49:09 +0300

    added tests

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,67 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CashbackHackServiceTest {
+
+        private CashbackHackService service = new CashbackHackService();
+
+        @Test
+        public void shouldCalculateWhenOneHundredRubleLackToThousand(){
+
+                int expected = 100;
+                int actual = service.remain(900);
+                Assert.assertEquals(actual, expected);
+        }
+
+        @Test
+        public void shouldCalculateWithFullThousand(){
+
+                int expected = 0;
+                int actual = service.remain(1000);
+                Assert.assertEquals(actual, expected);
+        }
+
+        @Test
+        public void shouldCalculateWhenNineHundredNinetyNineLackToTwoThousand(){
+
+                int expected = 999;
+                int actual = service.remain(1001);
+                Assert.assertEquals(actual, expected);
+        }
+
+        @Test
+        public void shouldCalculateWhenOneRubleLackToThousand(){
+
+                int expected = 1;
+                int actual = service.remain(999);
+                Assert.assertEquals(actual, expected);
+        }
+
+        @Test
+        public void shouldCalculateWhenNineHundredNinetyNineLackToThousand(){
+
+                int expected = 999;
+                int actual = service.remain(1);
+                Assert.assertEquals(actual, expected);
+        }
+
+        @Test
+        public void shouldCalculateWhenFullSumLack(){
+
+                int expected = 1000;
+                int actual = service.remain(0);
+                Assert.assertEquals(actual, expected);
+        }
+
+        @Test
+        public void shouldCalculateWithNegativeSum(){
+
+                int expected = 1001;
+                int actual = service.remain(-1);
+                Assert.assertEquals(actual, expected);
+        }
+
+
+}
